59f759a0cf5553d5bb450f80e149288eb56b48ce
Hernan Rodriguez <hrodriguezb5@ucentral.edu.co>
2026-05-19 16:09:31 -0500

    fix: eliminar carpeta Backend_Gestion_juridica

--- Backend_Gestion_juridica/src/main/java/co/vinni/abogados/repositorio/AbogadoRepositorio.java
+++
@@ -1,8 +0,0 @@
-package co.vinni.abogados.repositorio;
-
-import co.vinni.abogados.dominio.modelo.AbogadoEntity;
-
-// Interfaz que define las operaciones de base de datos para abogados
-public interface AbogadoRepositorio {
-    AbogadoEntity guardar(AbogadoEntity abogado);
-}
